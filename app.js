bc93e9858361f9f0a7c2b8419bbf5de1c23ae6a5
Sunveg <54052697+Sunveg@users.noreply.github.com>
2020-05-04 12:58:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const api_url = 'https://api.covid19india.org/data.json';
-
+let x = 92;
+let flag = 0;
 function getData() {
     fetch(api_url).then(function(response) {
         return response.json();
@@ -9,6 +10,7 @@ function getData() {
         document.getElementById('totcases').textContent = data.cases_time_series[pos - 1].totalconfirmed;
         document.getElementById('totrecov').textContent = data.cases_time_series[pos - 1].totalrecovered;
         document.getElementById('totdeaths').textContent = data.cases_time_series[pos - 1].totaldeceased;
+        document.getElementById('updatetime').textContent = 'last updated: ' + data.cases_time_series[pos - 1].date + ' 11:59 pm';
     })
     .catch(function(error) {
         console.log('OH! something went wrong');
